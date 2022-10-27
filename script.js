9272f538b5dc60a607b2c68998e3eaac5eb56df7
tornikekhvadagiani <113547924+tornikekhvadagiani@users.noreply.github.com>
2022-10-27 17:37:13 +0400

    dasasruli

---
+++ script.js
@@ -0,0 +1,44 @@
+let arithmeticMean =[ 4, 5, 35, 53, 22];
+console.log((arithmeticMean[0]+arithmeticMean[1]+arithmeticMean[2]+arithmeticMean[3]+arithmeticMean[4])/5);
+
+let list = [
+  {
+    name: "shorena",
+    age: 30,
+  },
+  {
+    name: "aleko",
+    age: 25,
+  },
+  {
+    name: "giorgi",
+    age: 60,
+  },
+  {
+    name: "დაჯო",
+    age: 25,
+  },
+  {
+    name: "sara",
+    age: 1,
+  }
+]
+
+let list2 = {
+    firstName : "tornike",
+    lastName : "khvadagiani",
+    age: 23,
+    phoneNumbers:[551542862, 598414387, 599000777],
+    address: [ "tbilisi","manjgalaZin N1"],
+    zipCode: 5,
+}
+const task4 = ` User name is ${list2.firstName}, user age is ${list2.age}, user address is ${list2.address[0].} `
+
+console.log(task4)
+
+
+
+
+
+
+
